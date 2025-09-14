a0dd1a82fc526e20c1204c2f2f42891e5dd6f86a
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-09-14 18:47:18 -0400

    tested image as input to nano

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import base64
 from llms import GPT_5_NANO
 
 load_dotenv()    
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
 
 def calculate_cost(input_tokens,output_tokens,input_cost,output_cost):
     onem = 1000000
@@ -21,8 +26,20 @@ def llm_call_nano(messages,model):
     cost = calculate_cost(res.usage.input_tokens,res.usage.output_tokens,model.input_cost,model.output_cost)
     return res,cost
 
+
+# Path to your image
+image_path = "Capture.JPG"
+base64_image = encode_image(image_path)
+
 messages = [
-    {"role":"user","content":"hi"}
+    {"role":"user","content":[
+            {   "type": "input_text", "text": "what's in this image?" },
+            {
+                "type": "input_image",
+                "image_url": f"data:image/jpeg;base64,{base64_image}",
+            },
+        ]
+    }
 ]
 res,cost = llm_call_nano(messages,GPT_5_NANO)
 print(res)
