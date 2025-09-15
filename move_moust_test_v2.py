bdf49f259b354ef321369e3f2ee8d32fd41fea1e
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-09-14 20:17:24 -0400

    move mouse instantly

--- move_moust_test_v2.py
+++ move_moust_test_v2.py
@@ -2,10 +2,10 @@ import time
 import pyautogui
 
 # Move to absolute position
-pyautogui.moveTo(200, 200, duration=0.25)
+pyautogui.moveTo(200, 200, duration=0.0)
 
 # Move relative
-pyautogui.move(100, 0, duration=0.2)
+pyautogui.move(100, 0, duration=0.0)
 
 # Smooth move to target
-pyautogui.moveTo(800, 500, duration=0.3)
+pyautogui.moveTo(800, 500, duration=0)
